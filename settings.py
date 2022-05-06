e1d8918d64eba18dfdba100093564af7ebb9193e
ldmz <ldmz.dev@gmail.com>
2022-05-06 19:58:25 +0300

    adding ship movements

--- settings.py
+++ settings.py
@@ -4,6 +4,8 @@ class Settings:
     def __init__(self):
         """Settings initialising"""
         # screen settings
-        self.screen_width = (1200)
-        self.screen_height= (800)
+        self.screen_width = 1200
+        self.screen_height= 800
         self.bg_color = (230, 230, 230)
+
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -1,17 +1,38 @@
 import pygame
 
+
 class Ship:
     """Clas for Ship"""
+
     def __init__(self, ai_game):
         """Initialise ship and its start position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        #load ship image and get its rect
+        # load ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        #make every ship in bottom, center part of screen
+
+        # make every ship in bottom, center part of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store ship horizzontal position
+        self.x = float(self.rect.x)
+
+        # movements
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update ship position"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship in its position"""
